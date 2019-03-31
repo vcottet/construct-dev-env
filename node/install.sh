369eea77db91b64b8f129699e1b5488722e89787
Vincent Cottet <vco2008@gmail.com>
2019-03-31 22:36:47 +0200

    workaround nvm installing in .config instead of .nvm

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
+[[ -d "$NVM_DIR" ]] || mkdir "$NVM_DIR"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
